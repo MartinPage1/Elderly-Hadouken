af8e82798a67738d8c30608d9230026a47e5710f
MartinPage1 <mppage1@ilstu.edu>
2021-10-27 15:07:02 -0500

    Added Betty

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,12 @@ public class GameManager : MonoBehaviour
 {
     public GameObject playerOnePrefab;
     public GameObject playerTwoPrefab;
+    public GameObject albertPrefab;
+    public GameObject bettyPrefab;
+    public GameObject archiPrefab;
+    public GameObject gertiePrefab;
+    public GameObject richardPrefab;
+    public GameObject edithPrefab;
     public TMP_Text playerOneHP;
     public TMP_Text playerTwoHP;
     public TMP_Text playerOnePP;
@@ -37,6 +43,30 @@ public class GameManager : MonoBehaviour
         {
             RestartScene();
         } 
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+           Instantiate(albertPrefab, new Vector3(-7.8f, -2.8f, 0), Quaternion.identity); 
+        } 
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+           Instantiate(bettyPrefab, new Vector3(-7.8f, -2.8f, 0), Quaternion.identity); 
+        } 
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+           Instantiate(archiPrefab, new Vector3(-7.8f, -2.8f, 0), Quaternion.identity); 
+        } 
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+           Instantiate(gertiePrefab, new Vector3(-7.8f, -2.8f, 0), Quaternion.identity); 
+        } 
+        if (Input.GetKeyDown(KeyCode.Keypad5))
+        {
+           Instantiate(richardPrefab, new Vector3(-7.8f, -2.8f, 0), Quaternion.identity); 
+        } 
+        if (Input.GetKeyDown(KeyCode.Keypad6))
+        {
+           Instantiate(edithPrefab, new Vector3(-7.8f, -2.8f, 0), Quaternion.identity); 
+        }
     }
     public void UpdateHealthP1(float health)
     {
